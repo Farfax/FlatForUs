5d144ae965a218c701c4c035631b7f9dd29d1768
Breeki <mfarfax@gmail.com>
2017-05-31 01:40:15 +0200

    Add filters, admin pages for models, validator exception for dates

--- Reserver/admin.py
+++ Reserver/admin.py
@@ -1,6 +1,62 @@
 from django.contrib import admin
 from .models import Town, Reserved, Flat
 
-myModels = [ Town, Reserved, Flat ]
-admin.site.register(myModels)
+class TownAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+
+class ReservedAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'isFree']
+    list_filter = ['isFree', 'flatName', 'userName', 'beginDate', 'endDate']
+    actions= ['set_free', 'set_busy']
+    fields = ['beginDate', 'endDate', 'flatName', 'userName', 'isFree']
+
+    def set_free(self, request, queryset):
+        updated = queryset.update(isFree = True)
+        if updated == 1:
+            message = "1 date was" 
+        else:
+            message= "%s dates were" % updated
+        self.message_user(request, "%s successfully set as free." % message)
+    
+    set_free.short_descripiton = "Mark selected dates as free"
+
+    def set_busy(self, request, queryset):
+        updated = queryset.update(isFree = False)
+        if updated == 1:
+            message = "1 date was" 
+        else:
+            message= "%s dates were" % updated 
+        self.message_user(request, "%s successfully set as busy." % message)
+   
+    set_busy.short_descripiton = "Mark selected dates as busy"
+
+class FlatAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'canRent']
+    list_filter = ['canRent', 'location']
+    actions = ['set_open', 'set_busy']
+    fields = ['flatName', 'location','openTime', 'openTimeEnd', 'canRent']
+
+    def set_open(self, request, queryset):
+        updated = queryset.update(canRent = True)
+        if updated == 1:
+            message = "1 Flat was" 
+        else:
+            message= "%s Flats were" % updated
+        self.message_user(request, "%s successfully set as rentable." % message)
+    set_open.short_descripiton = "Mark selected dates as rentable"
+
+    def set_busy(self, request, queryset):
+        updated = queryset.update(canRent = False)
+        if updated == 1:
+            message = "1 Flat was" 
+        else:
+            message= "%s Flats were" % updated
+        self.message_user(request, "%s successfully set as busy." % message)
+    set_busy.short_descripiton = "Mark selected flat as unRentable"
 # Register your models here.
+
+myModels = [ Town, Reserved, Flat ]
+admin.site.register(Town, TownAdmin)
+admin.site.register(Reserved, ReservedAdmin)
+admin.site.register(Flat, FlatAdmin)
+
--- Reserver/models.py
+++ Reserver/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.forms import ModelForm
+from django.core.exceptions import ValidationError
+from datetime import date
 
 class Town(models.Model):
     townName= models.CharField(max_length = 60)
@@ -7,17 +10,29 @@ class Town(models.Model):
         return str(self.townName)
 
 class Flat(models.Model):
-    flatName = models.CharField(max_length =60)
-    isAvailable= models.BooleanField(default=False)
-    location = models.CharField(max_length = 60)
+    flatName = models.CharField(max_length =60, unique=True)
+    canRent= models.BooleanField(default=True)
+    location = models.ForeignKey(Town, default=1)
+    openTime = models.DateField(default=date.today())
+    openTimeEnd = models.DateField(default=date.today())
+    #reservedDate = models.ManyToManyField('Reserved')
 
     def __str__(self):
         return str(self.flatName)
 
 class Reserved(models.Model):
-    beginDate = models.DateField()
-    endDate = models.DateField()
-    isFree = models.BooleanField(default=False)
+    beginDate = models.DateField(default=date.today())
+    endDate = models.DateField(default=date.today())
+    isFree = models.BooleanField(default=True)
     userName = models.CharField(max_length =60, default='Free')
-    flatName = models.CharField(max_length = 60)
+    flatName = models.ForeignKey('Flat', default=1)
+
+    def __str__(self):
+        return 'From '+str(self.beginDate)+' to '+str(self.endDate)+' : '+str(self.userName)
+
+    def save(self, *args, **kwargs):
+        if(self.beginDate > self.endDate):
+            raise ValidationError('Begin date cannot be bigger then end Date.')
+        else:
+            return super(Reserved, self).save(*args, **kwargs)
 # Create your models here.
